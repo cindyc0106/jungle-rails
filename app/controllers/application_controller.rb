79c1ff927c3005105061a3e20203c52e36f7d6ae
Cindy <cindy.c0106@gmail.com>
2022-10-20 16:21:07 +0000

    fixed bugs with routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,4 +41,5 @@ class ApplicationController < ActionController::Base
 end
 
 # Add a before_filter to any controller that you want to secure. This will force user's to login before they can see the actions in this controller
-# before_filter :authorize
+# # before_filter :authorize
+# before_action
