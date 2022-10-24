0a8c1f28d96251c8d1c828430e42a861ec8716c8
Cindy <cindy.c0106@gmail.com>
2022-10-24 15:57:24 +0000

    adding validation tests to product_spec

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
   describe 'Validations' do
     # validation tests/examples here
     it "saves successfully with all four fields set" do
@@ -10,5 +11,34 @@ RSpec.describe Product, type: :model do
       expect(@product).to be_valid
     end
 
+    it "does not validate successfully with missing name" do
+      @category = Category.new
+      @product = Product.new(name: nil, price: 25000, quantity: 5, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "does not validate successfully with missing price" do
+      @category = Category.new
+      @product = Product.new(name: "test", quantity: 5, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "does not validate successfully with missing quantity" do
+      @category = Category.new
+      @product = Product.new(name: "test", price: 25000, quantity: nil, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "does not validate successfully with missing category" do
+      @category = Category.new
+      @product = Product.new(name: "test", price: 25000, quantity: 5, category: nil)
+      @product.save
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
   end
+  
 end
